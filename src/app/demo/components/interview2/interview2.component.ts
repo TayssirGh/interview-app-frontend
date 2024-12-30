7cb58854b83181f9019d618eef3abbea9a4d4136
Rajett <rajet.stage@gmail.com>
2024-12-30 02:55:18 +0100

    interviewee : fixed textarea (without syntax coloring)

--- src/app/demo/components/interview2/interview2.component.ts
+++ src/app/demo/components/interview2/interview2.component.ts
@@ -3,7 +3,7 @@ import * as ace from 'ace-builds';
 
 @Component({
   selector: 'app-interview2',
-  templateUrl: './interview2.component.html',
+  templateUrl: './interview2.component.html'
 })
 export class Interview2Component implements OnInit, AfterViewInit {
   editor: any;
@@ -11,17 +11,20 @@ export class Interview2Component implements OnInit, AfterViewInit {
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    // Initialisation de l'éditeur ACE
+    // Configuration de base pour ACE Editor
+    ace.config.set('basePath', '/assets/ace'); // Assurez-vous que le chemin soit correct pour charger ACE
+
+    // Initialisation de l'éditeur
     this.editor = ace.edit('editor', {
-      theme: 'ace/theme/monokai', // Thème de l'éditeur
-      mode: 'ace/mode/javascript', // Mode de coloration syntaxique (ici JavaScript)
+      theme: 'ace/theme/monokai', // Choisir un thème
+      mode: 'ace/mode/javascript', // Choisir le mode (ici JavaScript)
       autoScrollEditorIntoView: true,
       wrap: true,
       fontSize: 14,
       showPrintMargin: false,
     });
 
-    // Définir le contenu par défaut
-    this.editor.setValue('// Votre code ici', -1); // Valeur par défaut
+    // Définir une valeur par défaut dans l'éditeur
+    this.editor.setValue('// Votre code ici', -1); 
   }
 }
